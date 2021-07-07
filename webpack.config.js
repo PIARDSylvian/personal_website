cd35d4a70e97f41eae531aacd5be8242e50d98f7
PIARD Sylvian <piard.sylvian@gmail.com>
2021-07-07 05:11:38 +0200

    add vueJs

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ Encore
 
     // will require an extra script tag for runtime.js
     // but, you probably want this, unless you're building a single-page app
-    .enableSingleRuntimeChunk()
+    .disableSingleRuntimeChunk()
 
     /*
      * FEATURE CONFIG
@@ -57,6 +57,9 @@ Encore
 
     // enables Sass/SCSS support
     .enableSassLoader()
+
+    // enables Vue.js support
+    .enableVueLoader()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()

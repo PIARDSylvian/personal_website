cbbd9042a557e5858cde2f3fdb08dd8b0fce2b13
PIARD Sylvian <piard.sylvian@gmail.com>
2021-07-13 21:39:28 +0200

    pw-api add category and base router

--- assets/vue/App.vue
+++ assets/vue/App.vue
@@ -2,7 +2,7 @@
     <div class="container-fluid">
         <div class="row">
             <nav class="col-12 col-sm-4 col-md-2 bg-light vh-col-sm-100">
-                <router-link class="align-items-center mb-md-0 me-md-auto link-dark text-decoration-none" to="/home">
+                <router-link class="align-items-center mb-md-0 me-md-auto link-dark text-decoration-none" to="/">
                     <span class="fs-4">App</span>
                 </router-link>
                 <hr>
@@ -16,29 +16,27 @@
                 </div>
                 <hr>
                 <ul class="nav nav-pills flex-column mb-auto">
-                    <router-link to="home" active-class="active" v-slot="{ navigate, isActive, isExactActive }" custom>
-                        <li class="nav-item" :class="[isActive && 'router-link-active', isExactActive && 'router-link-exact-active']">
-                            <a href="/home" @click="navigate" class="nav-link link-dark" :class="[isActive && 'active', isExactActive && 'active']">home</a>
+                    <router-link to="/" active-class="active" v-slot="{ href, navigate, isExactActive}" custom>
+                        <li class="nav-item">
+                            <a :href="href" @click="navigate" class="nav-link link-dark" :class="[isExactActive && 'active']">Home</a>
                         </li>
                     </router-link>
 
-                    <li v-if="isLoading" class="nav-item">
-                        <span v-if="isLoading" class="nav-link navbar-text link-dark text-decoration-none">Loading...</span>
+                    <li v-if="isLoading" class="nav-item disabled">
+                        <div class="d-flex justify-content-center mt-4">
+                            <span class="spinner-border" role="status"></span>
+                        </div>
                     </li>
 
-                    <li v-else-if="hasError" class="nav-item">
-                        <p class="alert alert-danger nav-link" role="alert">{{ error }}</p>
+                    <li v-else-if="hasError" class="nav-item disabled mt-3">
+                        <div class="alert alert-danger" role="alert">
+                            {{ error }}
+                        </div>
                     </li>
 
-                    <router-link v-for="link in menu" v-else :key="link" :to="link" active-class="active" v-slot="{ navigate, isActive, isExactActive }" custom >
-                        <li class="nav-item" :class="[isActive && 'router-link-active', isExactActive && 'router-link-exact-active']">
-                            <a :href="'/'+link" @click="navigate" class="nav-link link-dark" :class="[isActive && 'active', isExactActive && 'active']">{{ link }}</a>
-                        </li>
-                    </router-link>
-
-                    <router-link to="test" active-class="active" v-slot="{ navigate, isActive, isExactActive }" custom>
-                        <li class="nav-item" :class="[isActive && 'router-link-active', isExactActive && 'router-link-exact-active']">
-                            <a href="/test" @click="navigate" class="nav-link link-dark" :class="[isActive && 'active', isExactActive && 'active']">test</a>
+                    <router-link v-for="link in menu" v-else :key="link.name" :to="link.name|lowerCase" active-class="active" v-slot="{ navigate, isExactActive }" custom >
+                        <li class="nav-item">
+                            <a :href="'/'+link.name|lowerCase" @click="navigate" class="nav-link link-dark" :class="[isExactActive && 'active']">{{ link.name }}</a>
                         </li>
                     </router-link>
                 </ul>
@@ -72,14 +70,20 @@ export default {
         },
         menu() {
             let menu = this.$store.getters["menu/menu"];
-            menu.forEach(link => { 
-                router.addRoute({ path: '/'+link, name: link, component: Test }) 
+            menu.forEach(link => {
+                let url = '/'+link.name.replace(/\s/g, "").toLowerCase()
+                router.addRoute({ path: url, name: url, component: Test }) 
             })
             return menu
         }
     },
     created() {
         this.$store.dispatch("menu/getMenu");
+    },
+    filters : {
+        lowerCase : function (value) {
+            return value.replace(/\s/g, "").toLowerCase();
+        }
     }
 }
 </script>
--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -3,9 +3,9 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use App\Entity\Category;
 
 /**
  * @Rest\Route("/api")
@@ -14,12 +14,10 @@ class ApiController extends AbstractController
 {
     /**
      * @Rest\Get("/menu")
-     * @Rest\View(StatusCode = 201)
+     * @Rest\View(StatusCode = 201, serializerGroups={"menu"})
      */
     public function getMenu()
     {
-        sleep(2);
-        $test = ['aaa','bbb'];
-        return $test;
+        return $this->getDoctrine()->getRepository(Category::class)->findBy(array("active" => true));
     }
 }

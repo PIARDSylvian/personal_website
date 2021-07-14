6ba6ff41dad25073fbc1d38c67defca04915be0c
PIARD Sylvian <piard.sylvian@gmail.com>
2021-07-14 08:06:46 +0200

    pw-post add api postList post

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use App\Entity\Category;
+use App\Entity\Post;
 
 /**
  * @Rest\Route("/api")
@@ -14,10 +15,28 @@ class ApiController extends AbstractController
 {
     /**
      * @Rest\Get("/menu")
-     * @Rest\View(StatusCode = 201, serializerGroups={"menu"})
+     * @Rest\View(StatusCode = 200, serializerGroups={"menu"})
      */
     public function getMenu()
     {
         return $this->getDoctrine()->getRepository(Category::class)->findBy(array("active" => true));
+    }
+
+    /**
+     * @Rest\Get("/post")
+     * @Rest\View(StatusCode = 200, serializerGroups={"post"})
+     */
+    public function getPostList()
+    {
+        return $this->getDoctrine()->getRepository(Post::class)->findBy(array("active" => true));
+    }
+
+    /**
+     * @Rest\Get("/post/{slug}")
+     * @Rest\View(StatusCode = 200, serializerGroups={"post"})
+     */
+    public function getPost(String $slug)
+    {
+        return $this->getDoctrine()->getRepository(Post::class)->findOneBy(array("active" => true, "slug" => $slug));
     }
 }

63e85276c198944ab6d21fe40378b219d645fbd7
PIARD Sylvian <piard.sylvian@gmail.com>
2021-08-07 23:22:47 +0200

    remove post title unique

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -12,7 +12,6 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
  * @ORM\Entity(repositoryClass=PostRepository::class)
  * @ORM\HasLifecycleCallbacks()
  * @UniqueEntity("slug")
- * @UniqueEntity("title")
  */
 class Post
 {

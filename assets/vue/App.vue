5daa1eeae49bf115f6b34f5cb1e04979472279ed
PIARD Sylvian <piard.sylvian@gmail.com>
2021-08-09 02:32:59 +0200

    pw-front navside resize

--- assets/vue/App.vue
+++ assets/vue/App.vue
@@ -1,12 +1,12 @@
 <template>
 <div class="container-fluid">
     <div class="row">
-        <nav class="col-12 col-sm-4 col-md-2 navbar-expand-sm navbar-light bg-light">
+        <nav class="col-12 col-sm-4 col-md-3 col-lg-2 navbar-expand-sm navbar-light bg-light">
             <router-link class="d-flex justify-content-center img-fluid" to="/">
                 <img :src="'/build/logo-sp-min.png'" class="img-fluid my-2" alt="logo">
             </router-link>
             <div class="d-flex justify-content-between">
-                <div class="col-8 col-md-12">
+                <div class="col-8 col-sm-12">
                     <Search />
                 </div>
                 <button class="col-md navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -14,10 +14,10 @@
                 </button>
             </div>
             <hr>
-            <ul class="nav nav-pills flex-column mb-2 collapse navbar-collapse align-items-stretch" id="navbarSupportedContent">
+            <ul class="nav nav-pills flex-column mb-2 collapse navbar-collapse align-items-stretch fw-bold" id="navbarSupportedContent">
                 <router-link to="/" active-class="active" v-slot="{ href, navigate, isExactActive}" custom>
                     <li class="nav-item">
-                        <a :href="href" v-on="{ click: [navigate, closeMenu] }" class="nav-link link-dark" :class="[isExactActive && 'active gray']"><i class="bi bi-house-fill me-2 text-secondary"></i>Home</a>
+                        <a :href="href" v-on="{ click: [navigate, closeMenu] }" class="nav-link gray" :class="[isExactActive && 'active']"><i class="bi bi-house-fill me-2 text-secondary"></i>Home</a>
                     </li>
                 </router-link>
                 <li v-if="hasError" class="nav-item disabled mt-3">
@@ -32,12 +32,12 @@
                 </li>
                 <router-link v-for="link in menu" v-else :key="link.id" :to="{name:'category', params: {category: link.slug}}" active-class="active" v-slot="{href,navigate, isActive, isExactActive }" custom >
                     <li class="nav-item">
-                        <a :href="href" v-on="{ click: [navigate, closeMenu] }" class="nav-link link-dark" :class="[isActive && 'active' ,isExactActive && 'active', isActive && link.color]"><i class="bi bi-circle-fill me-2" :class="[!isActive && link.color]"></i>{{ link.name }}</a>
+                        <a :href="href" v-on="{ click: [navigate, closeMenu] }" class="nav-link" :class="[isActive && 'active' ,isExactActive && 'active', link.color]"><i class="bi bi-circle-fill me-2" :class="[!isActive && link.color]"></i>{{ link.name }}</a>
                     </li>
                 </router-link>
             </ul>
         </nav>
-        <main v-if="!isLoading && !hasError" class="col-12 col-sm-8 col-md-10">
+        <main v-if="!isLoading && !hasError" class="col-12 col-sm-8 col-md-9 col-lg-10">
             <router-view />
         </main>
     </div>

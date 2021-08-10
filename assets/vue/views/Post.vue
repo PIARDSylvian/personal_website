95350f36ad282a9c70bc20cee2492db2aec6e5f6
PIARD Sylvian <piard.sylvian@gmail.com>
2021-08-10 18:08:38 +0200

    date format fr

--- assets/vue/views/Post.vue
+++ assets/vue/views/Post.vue
@@ -23,6 +23,7 @@
 
 <script>
 import moment from 'moment';
+moment.locale('fr');
 
 export default {
 	name: "Post",
@@ -72,7 +73,7 @@ export default {
 	},
 	filters : {
 		dateFormat( $date ) {
-			return moment(String($date)).format('DD/MM/YYYY hh:mm');
+			return moment(String($date)).format('lll');
 		}
 	}
 };
